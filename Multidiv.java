202c700db8ea064b1449c8681bc9965cac1b8fa5
ItsDragon99 <dragonroliver@gmail.com>
2025-02-12 21:10:43 -0600

    feat: 'division con restas y multiplicacion con sumas"

---
+++ Multidiv.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Multidiv {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresar dos numeros para evaluar la multplicacion y division");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("La multiplicacion de " + a + " * " + b + " es: " + multi(a, b));
+        System.out.println("La division de " + a + " / " + b + " es: " + div(a, b));
+    }
+
+    public static int multi(int a, int b) {
+        if (b == 0) {
+            return 0;
+        }
+        return a + multi(a, b - 1);
+    }
+
+    public static int div(int a, int b) {
+        if (a < b) {
+            return 0;
+        }
+        return 1 + div(a - b, b);
+
+    }
+}
