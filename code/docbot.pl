cc738c9c9f9d2f4d952b422dfb8e2e1ac0418b25
ItsDragon99 <dragonroliver@gmail.com>
2025-05-28 17:14:45 -0600

    feat: 'agregue reglas'

--- code/docbot.pl
+++ code/docbot.pl
@@ -138,6 +138,7 @@ puede_afectar(Enfermedad, Genero) :-
 	genero(Genero),
 	afecta_a_genero(Enfermedad, Genero).
 
+
 template([tengo, s(_), '.'], [flagSintoma], [1]).
 template([presento, s(_), '.'], [flagSintoma], [1]).
 template([me, duele, s(_), '.'], [flagSintoma], [2]).
@@ -167,3 +168,159 @@ template([en, que, regiones, aparece, s(_), '?'], [flagRegion], [4]).
 
 template([hola], ['Hola, soy DocBot. ¿En qué puedo ayudarte?'], []).
 template([ayuda], ['Puedes preguntarme sobre síntomas, causas, especialistas, regiones o personas afectadas por ciertas enfermedades.'], []).
+
+
+respuesta_sintoma(Sintoma, R) :-
+	atom_string(AtomSintoma, Sintoma),
+	(   sintoma(AtomSintoma),
+	    findall(E, tiene_sintoma(E, AtomSintoma), Enfermedades),
+	    Enfermedades \= [],
+	    atomics_to_string(Enfermedades, ', ', EnfermedadesStr),
+	    string_concat('El síntoma que mencionas podría estar relacionado con: ', EnfermedadesStr, Temp),
+	    string_concat(Temp, '. Te recomendaría consultar a un médico.', Resp),
+	    string_to_atom(Resp, R)
+	;   
+	    R = 'No tengo información sobre ese síntoma específico. Te recomendaría consultar con un médico.'
+	).
+
+respuesta_enfermedad(Enfermedad, R) :-
+	atom_string(AtomEnfermedad, Enfermedad),
+	(   enfermedad(AtomEnfermedad),
+	    findall(S, tiene_sintoma(AtomEnfermedad, S), Sintomas),
+	    atomics_to_string(Sintomas, ', ', SintomasStr),
+	    findall(C, tiene_causa(AtomEnfermedad, C), Causas),
+	    atomics_to_string(Causas, ', ', CausasStr),
+	    string_concat('La ', Enfermedad, Temp1),
+	    string_concat(Temp1, ' presenta síntomas como: ', Temp2),
+	    string_concat(Temp2, SintomasStr, Temp3),
+	    string_concat(Temp3, '. Sus causas incluyen: ', Temp4),
+	    string_concat(Temp4, CausasStr, Resp),
+	    string_to_atom(Resp, R)
+	;   
+	    R = 'Lo siento, no tengo información sobre esa enfermedad.'
+	).
+
+respuesta_causa(Enfermedad, R) :-
+	atom_string(AtomEnfermedad, Enfermedad),
+	(   enfermedad(AtomEnfermedad),
+	    findall(C, tiene_causa(AtomEnfermedad, C), Causas),
+	    Causas \= [],
+	    atomics_to_string(Causas, ', ', CausasStr),
+	    string_concat('Las causas de ', Enfermedad, Temp1),
+	    string_concat(Temp1, ' incluyen: ', Temp2),
+	    string_concat(Temp2, CausasStr, Resp),
+	    string_to_atom(Resp, R)
+	;   
+	    R = 'Lo siento, no tengo información sobre las causas de esa condición.'
+	).
+
+respuesta_especialista(Enfermedad, R) :-
+	atom_string(AtomEnfermedad, Enfermedad),
+	(   enfermedad(AtomEnfermedad),
+	    findall(E, tratado_por(AtomEnfermedad, E), Especialistas),
+	    Especialistas \= [],
+	    atomics_to_string(Especialistas, ', ', EspecialistasStr),
+	    string_concat('Para ', Enfermedad, Temp1),
+	    string_concat(Temp1, ' deberías consultar con: ', Temp2),
+	    string_concat(Temp2, EspecialistasStr, Resp),
+	    string_to_atom(Resp, R)
+	;   
+	    R = 'No tengo información sobre qué especialista consultar para esa condición.'
+	).
+
+respuesta_afectados(Enfermedad, R) :-
+	atom_string(AtomEnfermedad, Enfermedad),
+	(   enfermedad(AtomEnfermedad),
+	    findall(G, afecta_a_genero(AtomEnfermedad, G), Generos),
+	    findall(E, afecta_a_edad(AtomEnfermedad, E), Edades),
+	    (   Generos \= [], Edades \= [] ->
+	        atomics_to_string(Generos, ', ', GenerosStr),
+	        atomics_to_string(Edades, ', ', EdadesStr),
+	        string_concat(Enfermedad, ' afecta principalmente a ', Temp1),
+	        string_concat(Temp1, GenerosStr, Temp2),
+	        string_concat(Temp2, ' de ', Temp3),
+	        string_concat(Temp3, EdadesStr, Resp),
+	        string_to_atom(Resp, R)
+	    ;   
+	        R = 'No tengo información detallada sobre quiénes son más afectados por esa condición.'
+	    )
+	;   
+	    R = 'No tengo información sobre esa condición.'
+	).
+
+respuesta_region(Enfermedad, R) :-
+	atom_string(AtomEnfermedad, Enfermedad),
+	(   enfermedad(AtomEnfermedad),
+	    findall(Reg, prevalente_en(AtomEnfermedad, Reg), Regiones),
+	    Regiones \= [],
+	    atomics_to_string(Regiones, ', ', RegionesStr),
+	    string_concat(Enfermedad, ' es más común en: ', Temp1),
+	    string_concat(Temp1, RegionesStr, Resp),
+	    string_to_atom(Resp, R)
+	;   
+	    R = 'No tengo información sobre las regiones donde es común esa condición.'
+	).
+
+
+
+replace0([I|_], Input, _, Resp, R):-
+	nth0(I, Input, Sintoma),
+	nth0(0, Resp, X),
+	X == flagSintoma,
+	respuesta_sintoma(Sintoma, R).
+
+replace0([I|_], Input, _, Resp, R):-
+	nth0(I, Input, Enfermedad),
+	nth0(0, Resp, X),
+	X == flagEnfermedad,
+	respuesta_enfermedad(Enfermedad, R).
+
+replace0([I|_], Input, _, Resp, R):-
+	nth0(I, Input, Enfermedad),
+	nth0(0, Resp, X),
+	X == flagCausa,
+	respuesta_causa(Enfermedad, R).
+
+replace0([I|_], Input, _, Resp, R):-
+	nth0(I, Input, Enfermedad),
+	nth0(0, Resp, X),
+	X == flagEspecialista,
+	respuesta_especialista(Enfermedad, R).
+
+replace0([I|_], Input, _, Resp, R):-
+	nth0(I, Input, Enfermedad),
+	nth0(0, Resp, X),
+	X == flagAfectados,
+	respuesta_afectados(Enfermedad, R).
+
+replace0([I|_], Input, _, Resp, R):-
+	nth0(I, Input, Enfermedad),
+	nth0(0, Resp, X),
+	X == flagRegion,
+	respuesta_region(Enfermedad, R).
+
+replace0([], _, _, Resp, R):- append(Resp, [], R),!.
+
+replace0([I|Index], Input, N, Resp, R):-
+	length(Index, M), M =:= 0,
+	nth0(I, Input, Atom),
+	select(N, Resp, Atom, R1), append(R1, [], R),!.
+
+replace0([I|Index], Input, N, Resp, R):-
+	nth0(I, Input, Atom),
+	length(Index, M), M > 0,
+	select(N, Resp, Atom, R1),
+	N1 is N + 1,
+	replace0(Index, Input, N1, R1, R),!.
+
+match([],[]).
+match([], _):- true.
+
+match([S|Stim],[I|Input]) :-
+	atom(S),
+	S == I,
+	match(Stim, Input),!.
+
+match([S|Stim],[_|Input]) :-
+	\+atom(S),
+	match(Stim, Input),!.
